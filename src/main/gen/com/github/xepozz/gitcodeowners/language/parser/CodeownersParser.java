f482c4ca7a31772fae48c1aa055ccb51a9c64a64
Dmitriy Derepko <xepozz@list.ru>
2025-02-10 19:41:29 +0300

    allow empty teams

--- src/main/gen/com/github/xepozz/gitcodeowners/language/parser/CodeownersParser.java
+++ src/main/gen/com/github/xepozz/gitcodeowners/language/parser/CodeownersParser.java
@@ -48,7 +48,7 @@ public class CodeownersParser implements PsiParser, LightPsiParser {
   }
 
   /* ********************************************************** */
-  // pattern team+
+  // pattern team*
   public static boolean definition(PsiBuilder b, int l) {
     if (!recursion_guard_(b, l, "definition")) return false;
     if (!nextTokenIs(b, TEXT)) return false;
@@ -61,19 +61,15 @@ public class CodeownersParser implements PsiParser, LightPsiParser {
     return r || p;
   }
 
-  // team+
+  // team*
   private static boolean definition_1(PsiBuilder b, int l) {
     if (!recursion_guard_(b, l, "definition_1")) return false;
-    boolean r;
-    Marker m = enter_section_(b);
-    r = team(b, l + 1);
-    while (r) {
+    while (true) {
       int c = current_position_(b);
       if (!team(b, l + 1)) break;
       if (!empty_element_parsed_guard_(b, "definition_1", c)) break;
     }
-    exit_section_(b, m, null, r);
-    return r;
+    return true;
   }
 
   /* ********************************************************** */
