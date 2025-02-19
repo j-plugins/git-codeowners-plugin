0db0e334a20c7bc4bfff7ca1ddb60bea422c31ca
Dmitriy Derepko <xepozz@list.ru>
2025-02-19 11:43:44 +0300

    feat: add documentation builder (comment folder)

---
+++ src/main/kotlin/com/github/xepozz/gitcodeowners/ide/documentation/CodeownersDocumentationProvider.kt
@@ -0,0 +1,85 @@
+package com.github.xepozz.gitcodeowners.ide.documentation
+
+import com.github.xepozz.crontab.ide.documentation.CodeownersDocumentationUtils
+import com.github.xepozz.gitcodeowners.language.CodeownersFile
+import com.github.xepozz.gitcodeowners.language.psi.CodeownersDefinition
+import com.intellij.extapi.psi.ASTDelegatePsiElement
+import com.intellij.lang.ASTNode
+import com.intellij.lang.documentation.DocumentationProvider
+import com.intellij.markdown.utils.doc.DocMarkdownToHtmlConverter
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiComment
+import com.intellij.psi.PsiDocCommentBase
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiTreeUtil
+import java.util.function.Consumer
+
+class CodeownersDocumentationProvider : DocumentationProvider {
+    override fun getUrlFor(element: PsiElement?, originalElement: PsiElement?): List<String>? {
+//        if (element !is CodeownersSchedule) return null
+//        return listOf(CodeownersGuruUtils.generateCrontabGuruUrl(element))
+        return null
+    }
+
+    override fun getCustomDocumentationElement(
+        editor: Editor,
+        file: PsiFile,
+        contextElement: PsiElement?,
+        targetOffset: Int
+    ): PsiElement? {
+        if (file !is CodeownersFile) return null
+
+        return PsiTreeUtil.findFirstParent(contextElement) { it is CodeownersDefinition }
+    }
+
+    /**
+     * Extracts the key, value, file and documentation comment of a Simple key/value entry and returns
+     * a formatted representation of the information.
+     */
+    override fun generateDoc(element: PsiElement?, originalElement: PsiElement?): String? = null
+
+    override fun getQuickNavigateInfo(element: PsiElement?, originalElement: PsiElement?): String? = null
+
+    override fun generateRenderedDoc(comment: PsiDocCommentBase) = markdownToHtml(comment.text, comment.project)
+
+    fun markdownToHtml(string: String, project: Project) = string
+        .split("\n")
+        .joinToString("\n") { it.replaceFirst(Regex("#+\\s+"), "") }
+        .let { DocMarkdownToHtmlConverter.convert(project, it) }
+
+    override fun findDocComment(file: PsiFile, range: TextRange): PsiDocCommentBase? {
+        val element = file.findElementAt(range.startOffset) as? PsiComment ?: return null
+
+        return object : PsiCommentDelegate(element), PsiDocCommentBase {
+            override fun getOwner() = element
+        }
+    }
+
+    override fun collectDocComments(file: PsiFile, sink: Consumer<in PsiDocCommentBase>) {
+        buildList {
+            addAll(PsiTreeUtil.findChildrenOfType(file, CodeownersDefinition::class.java))
+        }
+            .forEach { expression ->
+//                println("comments for $expression")
+                val comment = CodeownersDocumentationUtils.findContextualDocumentationElement(expression) ?: return@forEach
+//                println("$comment for $expression")
+                sink.accept(object : PsiCommentDelegate(comment), PsiDocCommentBase {
+                    override fun getOwner() = expression
+                })
+            }
+    }
+
+    override fun generateHoverDoc(element: PsiElement, originalElement: PsiElement?) =
+        generateDoc(element, originalElement)
+}
+
+open class PsiCommentDelegate(val myComment: PsiComment) : PsiComment, ASTDelegatePsiElement() {
+    override fun getTokenType() = myComment.tokenType
+
+    override fun getParent(): PsiElement? = myComment.parent
+
+    override fun getNode(): ASTNode = myComment.node
+}
---
+++ src/main/kotlin/com/github/xepozz/gitcodeowners/ide/documentation/CodeownersDocumentationUtils.kt
@@ -0,0 +1,26 @@
+package com.github.xepozz.crontab.ide.documentation
+
+import com.github.xepozz.gitcodeowners.language.psi.CodeownersTypes
+import com.intellij.psi.PsiComment
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiWhiteSpace
+import com.intellij.psi.util.elementType
+
+object CodeownersDocumentationUtils {
+    fun findCrontabElementDocumentation(element: PsiElement?): String =
+        findContextualDocumentationElement(element)?.text?.replaceFirst("[# ]+", "") ?: ""
+
+    fun findContextualDocumentationElement(element: PsiElement?): PsiComment? {
+        if (element == null) return null
+
+        var element = element.prevSibling
+        while (element is PsiComment || element is PsiWhiteSpace || element.elementType == CodeownersTypes.EOL) {
+            if (element is PsiComment) {
+                return element
+            }
+            element = element.prevSibling
+        }
+
+        return null
+    }
+}
