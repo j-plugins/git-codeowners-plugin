7d1e67468f1e7521d1dd34bfdd1762c8692bbeb4
Dmitriy Derepko <xepozz@list.ru>
2025-02-24 15:47:10 +0300

    rewrite md support

--- src/main/kotlin/com/github/xepozz/gitcodeowners/ide/documentation/CodeownersDocumentationProvider.kt
+++ src/main/kotlin/com/github/xepozz/gitcodeowners/ide/documentation/CodeownersDocumentationProvider.kt
@@ -6,7 +6,6 @@ import com.github.xepozz.gitcodeowners.language.psi.CodeownersDefinition
 import com.intellij.extapi.psi.ASTDelegatePsiElement
 import com.intellij.lang.ASTNode
 import com.intellij.lang.documentation.DocumentationProvider
-import com.intellij.markdown.utils.doc.DocMarkdownToHtmlConverter
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
@@ -45,10 +44,25 @@ class CodeownersDocumentationProvider : DocumentationProvider {
 
     override fun generateRenderedDoc(comment: PsiDocCommentBase) = markdownToHtml(comment.text, comment.project)
 
-    fun markdownToHtml(string: String, project: Project) = string
-        .split("\n")
-        .joinToString("\n") { it.replaceFirst(Regex("#+\\s+"), "") }
-        .let { DocMarkdownToHtmlConverter.convert(project, it) }
+    fun markdownToHtml(string: String, project: Project): String {
+        val markdownText = string
+            .split("\n")
+            .joinToString("\n") { it.replaceFirst(Regex("#+\\s+"), "") }
+
+        return try {
+            // Dynamically find the class
+            val clazz = Class.forName("com.intellij.markdown.utils.doc.DocMarkdownToHtmlConverter")
+
+            // Find the 'convert' method (assuming it's `static fun convert(project, text)`)
+            val method = clazz.getMethod("convert", Project::class.java, String::class.java)
+
+            // Invoke the static method
+            method.invoke(null, project, markdownText) as String
+        } catch (e: ClassNotFoundException) {
+//            println("Markdown converter not available, returning plain text.")
+            markdownText
+        }
+    }
 
     override fun findDocComment(file: PsiFile, range: TextRange): PsiDocCommentBase? {
         val element = file.findElementAt(range.startOffset) as? PsiComment ?: return null
