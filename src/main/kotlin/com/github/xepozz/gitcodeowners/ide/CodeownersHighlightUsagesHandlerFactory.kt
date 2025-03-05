a1a526ceff6091e1c03b4ee87592d7c3a78eddf0
Dmitriy Derepko <xepozz@list.ru>
2025-03-05 10:04:58 +0300

    fix highlighting bug

--- src/main/kotlin/com/github/xepozz/gitcodeowners/ide/CodeownersHighlightUsagesHandlerFactory.kt
+++ src/main/kotlin/com/github/xepozz/gitcodeowners/ide/CodeownersHighlightUsagesHandlerFactory.kt
@@ -1,38 +1,39 @@
 package com.github.xepozz.gitcodeowners.ide
 
-import com.github.xepozz.gitcodeowners.language.psi.CodeownersTeam
+import com.github.xepozz.gitcodeowners.language.CodeownersFile
 import com.github.xepozz.gitcodeowners.language.psi.impl.CodeownersElementImpl
-import com.intellij.codeInsight.highlighting.HighlightUsagesHandler
 import com.intellij.codeInsight.highlighting.HighlightUsagesHandlerBase
-import com.intellij.codeInsight.highlighting.HighlightUsagesHandlerFactory
 import com.intellij.codeInsight.highlighting.HighlightUsagesHandlerFactoryBase
 import com.intellij.openapi.editor.Editor
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.Consumer
-import org.eclipse.lsp4j.DocumentHighlightKind
 
 class CodeownersHighlightUsagesHandlerFactory : HighlightUsagesHandlerFactoryBase() {
     override fun createHighlightUsagesHandler(
         editor: Editor,
         file: PsiFile,
         target: PsiElement
-    ) = object : HighlightUsagesHandlerBase<CodeownersElementImpl>(editor, file) {
-        override fun getTargets() =
-            PsiTreeUtil.findChildrenOfType(file, CodeownersElementImpl::class.java)
-                .filterNotNull()
-                .toList()
+    ): HighlightUsagesHandlerBase<CodeownersElementImpl>? {
+        val file = file as? CodeownersFile ?: return null
 
-        override fun selectTargets(
-            targets: List<CodeownersElementImpl?>,
-            selectionConsumer: Consumer<in MutableList<out CodeownersElementImpl>>
-        ) {
+        return object : HighlightUsagesHandlerBase<CodeownersElementImpl>(editor, file) {
+            override fun getTargets() =
+                PsiTreeUtil.findChildrenOfType(file, CodeownersElementImpl::class.java)
+                    .filterNotNull()
+                    .toList()
+
+            override fun selectTargets(
+                targets: List<CodeownersElementImpl?>,
+                selectionConsumer: Consumer<in MutableList<out CodeownersElementImpl>>
+            ) {
+            }
+
+            override fun computeUsages(targets: List<CodeownersElementImpl>) =
+                targets
+                    .filter { it::class == target.parent::class && it.text == target.text }
+                    .forEach(::addOccurrence)
         }
-
-        override fun computeUsages(targets: List<CodeownersElementImpl>) =
-            targets
-                .filter { it::class == target.parent::class && it.text == target.text }
-                .forEach(::addOccurrence)
     }
 }
--- src/main/kotlin/com/github/xepozz/gitcodeowners/language/CodeownersAnnotator.kt
+++ src/main/kotlin/com/github/xepozz/gitcodeowners/language/CodeownersAnnotator.kt
@@ -1,8 +1,5 @@
 package com.github.xepozz.gitcodeowners.language
 
-//import com.github.xepozz.gitcodeowners.language.psi.CodeownersTimePointer
-//import com.github.xepozz.gitcodeowners.language.psi.CodeownersVariableName
-//import com.github.xepozz.gitcodeowners.language.psi.CodeownersVariableValue
 import com.github.xepozz.gitcodeowners.language.psi.CodeownersPattern
 import com.github.xepozz.gitcodeowners.language.psi.CodeownersTeam
 import com.intellij.lang.annotation.AnnotationHolder
@@ -28,20 +25,6 @@ class CodeownersAnnotator : Annotator {
                     .textAttributes(PARAMETERS_HIGHLIGHT)
                     .create()
             }
-//
-//            is CodeownersVariableValue -> {
-//                holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
-//                    .range(element.textRange)
-//                    .textAttributes(STRING_HIGHLIGHT)
-//                    .create()
-//            }
-
-//            is CodeownersCommand -> {
-//                holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
-//                    .range(element.textRange)
-//                    .textAttributes(COMMAND_HIGHLIGHT)
-//                    .create()
-//            }
         }
     }
 
@@ -54,17 +37,5 @@ class CodeownersAnnotator : Annotator {
             "GITCODEOWNERS_IDENTIFIER",
             DefaultLanguageHighlighterColors.KEYWORD,
         )
-//        private val STRING_HIGHLIGHT = TextAttributesKey.createTextAttributesKey(
-//            "GITCODEOWNERS_STRING",
-//            DefaultLanguageHighlighterColors.STRING,
-//        )
-//        private val OPERATION_HIGHLIGHT = TextAttributesKey.createTextAttributesKey(
-//            "GITCODEOWNERS_OPERATION",
-//            DefaultLanguageHighlighterColors.OPERATION_SIGN,
-//        )
-//        private val COMMAND_HIGHLIGHT = TextAttributesKey.createTextAttributesKey(
-//            "GITCODEOWNERS_COMMAND",
-//            DefaultLanguageHighlighterColors.STRING,
-//        )
     }
 }
